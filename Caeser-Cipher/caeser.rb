dccbfae7a684f7e32359a46ef4a0cb4aae342bdc
Dale Belcham <dandelionwishes@protonmail.ch>
2023-03-26 14:06:15 +0200

    working on substrings code

--- Caeser-Cipher/caeser.rb
+++ Caeser-Cipher/caeser.rb
@@ -1,35 +1,31 @@
-string_in = "Butterfly House"
+string_in = 'Butterfly House'
 
 puts "String to encode: #{string_in}"
 
 transform_one = string_in.bytes
 
-#p transform_one
+# p transform_one
 
 def cipher(string, change)
-  
   puts "Shift factor: #{change}"
 
   transform_two = string.map do |character|
-
     new_character = character + change
 
-      if character <= 90 && character >= 65
-        new_character > 90 ? new_character -= 26 : new_character
-      elsif character <= 122 && character >= 97 
-        new_character > 122 ? new_character -= 26 : new_character
-      else 
-        character
-      end
-
+    if character <= 90 && character >= 65
+      new_character > 90 ? new_character -= 26 : new_character # rubocop:disable Lint/UselessAssignment
+    elsif character <= 122 && character >= 97
+      new_character > 122 ? new_character -= 26 : new_character # rubocop:disable Lint/UselessAssignment
+    else
+      character
+    end
   end
 
-  #p transform_two
+  # p transform_two
 
   encoded = transform_two.map { |ascii_number| ascii_number.chr }.join
 
   puts "Encoded string: #{encoded}"
-
 end
 
 cipher(transform_one, 3)
---
+++ Sub-Strings/sub_strings.rb
@@ -0,0 +1,13 @@
+dictionary = ['the', 'dog', 'great', 'a']
+
+def substrings(string, array)
+  final_array = string.split.map do |word|
+    array.grep(word)
+  end
+  final_array.flatten!
+  final_hash = final_array.each_with_object(Hash.new(0)) do |each_occurence, result|
+    result[each_occurence] += 1
+  end
+end
+
+p substrings('the dog and a blue dog went to the dog', dictionary)
